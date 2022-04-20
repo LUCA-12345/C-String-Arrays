ece3ec39dac33b915300889eb294f06af18cd903
Luke-Tascone <99374613+Luke-Tascone@users.noreply.github.com>
2022-04-20 14:58:05 -0400

    Add files via upload

---
+++ C-String-Arrays.cpp
@@ -0,0 +1,77 @@
+// Lab10B
+// Programmer : Luke Tascone 
+
+#include <iostream>
+#include <string> 
+#include <stdlib.h>
+
+char validateInput (char);
+void changeVowels ( char  * , char);
+
+
+using namespace std;
+
+int main()
+{
+    const int SIZE = 101; 
+    char userSentence[SIZE];
+
+    string sentence; 
+    char vowel;  
+
+    cout << "Enter a sentence: ";
+    cin.getline(userSentence, SIZE);   
+    cout << endl;
+
+    while ( vowel != '/')
+    {
+        cout << "Enter a vowel or / to quit: ";
+        cin >> vowel; 
+        cout << endl; 
+
+        if (vowel == '/' )  
+        {  
+            cout << "Goodbye!" << endl;
+            return 0;                                
+        }
+        else if (vowel != '/')
+        {
+           validateInput(vowel);
+        }
+
+        changeVowels(userSentence,vowel);
+
+        cout << userSentence << endl;
+    }
+
+    return 0;
+}
+
+char validateInput (char input)                 
+{
+    if ( input == 'a' || input ==  'A' || input ==  'e' || input ==  'E' || input ==  'i' || input ==  'I' || input ==  'o' || input ==  'O' || input ==  'u' || input ==  'U')
+    {
+        return input;
+    }
+    else 
+    {
+        cout << "You did not enter a vowel, please enter a, e, i, o, or u: ";
+        cin >> input;
+    }
+
+    return input;
+}
+
+
+void changeVowels ( char *strPtr, char validVowel) 
+{
+    while (*strPtr != '\0' )
+    {
+        if ( *strPtr == 'a' || *strPtr == 'A' || *strPtr ==  'e' || *strPtr ==  'E' || *strPtr ==  'i' || *strPtr ==   'I' || *strPtr ==   'o' || *strPtr ==   'O' || *strPtr ==   'u' || *strPtr ==   'U' )
+        {
+            *strPtr = validVowel;
+        }
+        strPtr++;
+    }
+}
+
